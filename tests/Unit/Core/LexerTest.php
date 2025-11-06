efa65f901fe000ab043b0c3ea1d09add37a1747e
rok <post@robert-kummer.de>
2025-11-06 18:45:11 +0100

    update LexerTest: refine exception message assertion to include offending snippet for improved clarity

--- tests/Unit/Core/LexerTest.php
+++ tests/Unit/Core/LexerTest.php
@@ -46,7 +46,7 @@ it('throws exception when token is not recognized', function (): void {
     try {
         $lexer->tokenize($phpScriptCode);
     } catch (LexerException $e) {
-        expect($e->getMessage())->toBe('Unknown character or syntax error at line 2, column 5.');
+        expect($e->getMessage())->toBe('Unknown character or syntax error `@;⏎c = 3;` at line 2, column 5.');
     }
 });
 

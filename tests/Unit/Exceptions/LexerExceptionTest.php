e94a0404097542506547ff1ab73633b77a9b8dfa
rok <post@robert-kummer.de>
2025-11-06 18:45:05 +0100

    add LexerExceptionTest case for detailed message formatting with line and column info

--- tests/Unit/Exceptions/LexerExceptionTest.php
+++ tests/Unit/Exceptions/LexerExceptionTest.php
@@ -9,5 +9,13 @@ it('creates exception with correct message', function (): void {
     $exception = LexerException::unknownCharOrSyntaxError($remainingCode);
 
     expect($exception)->toBeInstanceOf(LexerException::class)
-        ->and($exception->getMessage())->toBe('Unknown char or syntax error: @#$%');
+        ->and($exception->getMessage())->toBe('Unknown character or syntax error `@#$%` at line 0, column 0.');
+});
+
+it('creates exception with abbreviated token message', function (): void {
+    $remainingCode = '@#$% ' . "\n" . ' foo.bar()';
+    $exception = LexerException::unknownCharOrSyntaxError($remainingCode);
+
+    expect($exception)->toBeInstanceOf(LexerException::class)
+        ->and($exception->getMessage())->toBe('Unknown character or syntax error `@#$%` at line 0, column 0.');
 });

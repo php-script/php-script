c38d8bce5f664add441e418ea11d93eea955bf2a
rok <post@robert-kummer.de>
2025-11-10 20:52:08 +0100

    add unit tests for Ast nodes: validate `UnaryOperation` and `IfStatement` AST structures with detailed assertions

---
+++ tests/Unit/Ast/UnaryOperationTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use PhpScript\Ast\Literal;
+use PhpScript\Ast\UnaryOperation;
+use PhpScript\Core\Token;
+use PhpScript\Core\TokenType;
+
+it('can create a unary operation', function () {
+    $right = new Literal(true, new Token(TokenType::T_TRUE, 'true', 1, 1, 0));
+    $unary = new UnaryOperation(TokenType::T_BANG, $right, new Token(TokenType::T_BANG, '!', 1, 1, 0));
+
+    expect($unary->operator)->toBe(TokenType::T_BANG);
+    expect($unary->right)->toBe($right);
+    expect($unary->toArray())->toBe([
+        'type' => UnaryOperation::class,
+        'operator' => TokenType::T_BANG->value,
+        'right' => $right->toArray(),
+    ]);
+});
